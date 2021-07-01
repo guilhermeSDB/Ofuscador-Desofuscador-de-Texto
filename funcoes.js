ac2691d1adab08f656df91b8602caa19684c8574
Guilherme Barros <Guilhermescarmagnani@outlook.com>
2021-07-01 19:33:22 -0300

    att 19:33

--- funcoes.js
+++ funcoes.js
@@ -16,5 +16,17 @@ function desofuscar(){
     document.getElementById("textarea-ofuscado").value = inputText
 }
 
+function copyClipboard(){
+    var copyText = document.getElementById("textarea-ofuscado");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    document.execCommand("copy");
 
-    
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Copied: " + copyText.value;
+}
+
+function outFunc() {
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Copy to clipboard";
+  }
